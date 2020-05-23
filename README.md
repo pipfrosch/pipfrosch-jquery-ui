146e9c4350775142f9189e79de33a389cc3a20ae
pipfrosch <pipfroschpress@gmail.com>
2020-05-23 04:45:46 -0700

    started work

---
+++ README.md
@@ -0,0 +1,23 @@
+Pipfrosch jQuery UI
+===================
+
+This will not be seriously worked on much until I need it. And I may not.
+
+If I end up using jQuery UI within piptheme then I will need this, so I am
+slowly coding it so that if I do need it, it is further along.
+
+Nutshell is it will bring an updated CDN compatible jQueryUI to WordPress.
+
+A new version of jQuery UI is coming soon that will get rid of current
+deprecation warnings and definitely will not need the jQuery Migrate plugin to
+work with current jQuery (I think 1.12.1 also does not but I'm not sure)
+
+--------------
+
+WordPress has an older version of jQuery UI and broken up into lots of little
+scripts but that is not how content distribution networks provide it, they tend
+to provide it as a complete single script.
+
+Even though single script means a larger file is loaded than needed, it also
+means less files to download and the size issue is largely mitigated by caching
+of the file from a Public CDN anyway.
---
+++ inc/functions.php
@@ -0,0 +1,74 @@
+<?php
+
+if ( ! defined( 'PIPJQUI_PLUGIN_WEBPATH' ) ) { exit; }
+
+function pipjq_update_wpcore_jqueryui() {
+
+  $core = array(         'jquery-ui-widget',
+                         'jquery-ui-position');
+
+  $core_supp = array(    'jquery-ui-keycode',
+                         'jquery-ui-disable-selection',
+                         'jquery-ui-disableselection',
+                         'jquery-ui-form-reset-mixin',
+                         'jquery-ui-labels',
+                         'jquery-ui-scroll-parent',
+                         'jquery-ui-scrollparent',
+                         'jquery-ui-unique-id',
+                         'jquery-ui-uniqueid');
+  //?? :data Selector -- :focusable Selector -- :tabbable Selector ??
+
+  $interactions = array( 'jquery-ui-draggable',
+                         'jquery-ui-droppable',
+                         'jquery-ui-resizeable',
+                         'jquery-ui-selectable',
+                         'jquery-ui-sortable');
+
+  $widgets = array(      'jquery-ui-accordion',
+                         'jquery-ui-autocomplete',
+                         'jquery-ui-button',
+                         'jquery-ui-datepicker',
+                         'jquery-ui-dialog',
+                         'jquery-ui-menu',
+                         'jquery-ui-mouse',
+                         'jquery-ui-progressbar',
+                         'jquery-ui-selectmenu',
+                         'jquery-ui-slider',
+                         'jquery-ui-spinner',
+                         'jquery-ui-tabs',
+                         'jquery-ui-tooltip',
+
+  $widgets_supp = array( 'jquery-ui-checkboxradio',
+                         'jquery-ui-controlgroup');
+
+  $effects = array(      'jquery-ui-effects-core',
+                         'jquery-ui-effects-blind',
+                         'jquery-ui-effects-bounce',
+                         'jquery-ui-effects-clip',
+                         'jquery-ui-effects-drop',
+                         'jquery-ui-effects-explode',
+                         'jquery-ui-effects-fade',
+                         'jquery-ui-effects-fold',
+                         'jquery-ui-effects-highlight',
+                         'jquery-ui-effects-puff',
+                         'jquery-ui-effects-pulsate',
+                         'jquery-ui-effects-scale',
+                         'jquery-ui-effects-shake',
+                         'jquery-ui-effects-size',
+                         'jquery-ui-effects-slide',
+                         'jquery-ui-effects-transfer');
+
+  $components = array_merge($core, $interactions, $widgets, $effects);
+  $alias_components = array_merge($components, $core_supp, $widgets_supp);
+
+  wp_deregister_script( 'jquery-ui-core' );
+  foreach( $components as $component ) {
+    wp_deregister_script( $component );
+  }
+  wp_register_script( 'jquery-ui-core', $srcuri->jqueryui, array( 'jquery-core' ), null );
+  // set up the aliases
+  foreach( $alias_components as $component ) {
+    wp_register_script( $component, null, array( 'jquery-ui-core' ), null );
+  }
+
+}
