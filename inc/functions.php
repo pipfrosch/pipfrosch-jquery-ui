9167c54d1734372770db4e9bb0fb4a6da2184695
pipfrosch <pipfroschpress@gmail.com>
2020-05-24 08:13:39 -0700

    more work on plugin

--- inc/functions.php
+++ inc/functions.php
@@ -47,6 +47,42 @@ function pipjqui_theme_to_stub( string $theme ): string
 }
 
 /**
+ * Get an option as boolean value.
+ *
+ * This function queries the option and returns a boolean value representing
+ *  the option if the option has been set and returns a boolean default if the
+ *  option has not been set. It also sets the option to a string equivalent of
+ *  the boolean default (a "0" for false or a "1" for true) if the option is
+ *  not set.
+ *
+ * @param string The name of the option to query.
+ * @param bool   The default value to return and set if the option is not set.
+ *
+ * @return bool
+ */
+function pipjqui_get_option_as_boolean( string $option, bool $default = true ) {
+  $test = get_option( $option );
+  if ( is_bool( $test ) ) {
+    if ( $test ) {
+       update_option( $option, '1');
+       return $test;
+    } else {
+       $value = '0';
+       if ( $default ) {
+         $value = '1';
+       }
+       add_option( $option, $value );
+       return $default;
+    }
+  }
+  $q = intval( $test );
+  if ( $q === 0 ) {
+    return false;
+  }
+  return true;
+}
+
+/**
  * Sanitize CDN host string
  *
  * This WordPress plugin uses a fixed set of public Content Distribution Networks.
@@ -78,8 +114,11 @@ function pipjqui_sanitize_cdnhost( string $input ) {
 }
 
 function pipjqui_get_cdnhost_option() {
-  if ( function_exists( 'pipjq_get_cdnhost_option' ) {
-    $default = pipjqui_sanitize_cdnhost( pipjq_get_cdnhost_option() );
+  // see if preferred CDN from Pipfrosch jQuery has been set and default to
+  //  that if it has been.
+  $test = get_option( 'pipjq_cdnhost' ); // this option not set by this plugin
+  if ( is_string( $test ) ) {
+    $default = pipjqui_sanitize_cdnhost( $test );
   } else {
     $default = pipjqui_sanitize_cdnhost( 'use default' );
   }
@@ -120,12 +159,9 @@ function pipjqui_get_cdnhost(): string
  */
 function pipjqui_register_themes(): void
 {
-  $cdnhost = 'localhost';
-  if ( pipjqui_get_option_as_boolean( 'pipjqui_csscdn', false) ) {
-    $cdnhost = pipjqui_get_cdnhost();
-  }
+  $cdnhost = pipjqui_get_cdnhost();
   // jquery-ui-theme-base
-  $src = PIPJQ_PLUGIN_WEBPATH . '/themes/base/jquery-ui.min.css';
+  $src = PIPJQ_PLUGIN_WEBPATH . 'themes/base/jquery-ui.min.css';
   // when serving locally include non-null version parameter
   wp_register_style( 'jquery-ui-base', $src, array(), PIPJQUIV );
   
@@ -184,7 +220,7 @@ function pipjqui_register_themes(): void
         wp_register_style( $handle, $src, array('jquery-ui-base'), null );
         break;
       default:
-        $src = PIPJQ_PLUGIN_WEBPATH . '/themes/' . $stub . '/jquery-ui.min.css';
+        $src = PIPJQ_PLUGIN_WEBPATH . 'themes/' . $stub . '/jquery-ui.min.css';
         // when serving locally include non-null version parameter
         //  when serving locally, we do not need base as fall-back dep
         wp_register_style( $handle, $src, array(''), PIPJQUIV );
@@ -194,9 +230,8 @@ function pipjqui_register_themes(): void
   $handle = 'jquery-ui-base';
   wp_register_style( 'jquery-ui-theme-active', false, array( $handle ), null );
   
-  /* register aliases for what base/themes provide */
-  $base = array( 'core',
-                 'accordion',
+  /* register aliases for what standard themes provide - TODO I think this borked */
+  $base = array( 'accordion',
                  'autocomplete',
                  'button',
                  'datepicker',
@@ -256,9 +291,93 @@ function pipjqui_script_src( string $cdnhost="localhost" )
   return $rs;
 }
 
+/**
+ * Fallback for a CDN failure.
+ *
+ * In the event that a client can not retrieve the needed jQuery file from a CDN or
+ *  it retrieves the file but the SRI does not match, a website will be broken without
+ *  a fallback. This function provides a small HTML snippet for a script that verifies
+ *  retrieval of the jQuery library or the jQuery Migrate plugin was successful, and
+ *  instructs the client to download the copy served from the local website if it was
+ *  not.
+ *
+ * @return string
+ */
+function pipjq_fallback_for_cdn_failure(): string
+{
+  $html = '<script>' . PHP_EOL . '  // Fallback to load locally if CDN fails' . PHP_EOL;
+  $html .= '  if (typeof jQuery.ui == \'undefined\') {' . PHP_EOL;
+  $html .= '    document.write(\'<script src="' . PIPJQUI_PLUGIN_WEBPATH . 'jquery-ui.min.js?ver=' . PIPJQUIV . '"><\/script>\');' . PHP_EOL;
+  $html .= '  }' . PHP_EOL;
+  $html .= '</script>' . PHP_EOL;
+  return $html;
+}
+
+/**
+ * Add the SRI and CrossOrigin attributes
+ *
+ * The WordPress core function `wp_register_script()` does not provide a means
+ *  for adding attributes to a script node. When a script is served from a third
+ *  party resource, it really needs to have both an `integrity` and a
+ *  `crossorigin` attribute set so the browser can validate both the integrity of
+ *  the resource being downloaded from the third party, and what authentication
+ *  should be performed (always anonymous for a public CDN, which at least with
+ *  some browsers means cookies are not set or sent even if they exist. Yay for
+ *  privacy). This function is used as a callback in a the WordPress
+ *  `script_loader_tag` to rewrite the <script></script> node with the appropriate
+ *  attributes for both SRI and CrossOrigin, along with the fallback HTML snippet
+ *  from the `pipjqui_fallback_for_cdn_failure()` function.
+ *
+ * @param string The original version of the script tag.
+ * @param string The handle that was used to register the script associated with
+ *               the tag in the first parameter.
+ * @param string The contents of the `src` attribute in the tag from the first
+ *               parameter.
+ *
+ * @return string
+ */
+function pipjqui_add_sri_attributes( string $tag, string $handle, string $source ): string
+{
+  if ( $handle === 'jquery-ui-core' ) {
+    $html = pipjq_fallback_for_cdn_failure();
+    return '<script src="' . $source . '" integrity="' . PIPJQUIVSRI . '" crossorigin="anonymous"></script>' . PHP_EOL . $html;
+  }
+  return $tag;
+}
+
+/**
+ * Add CrossOrigin attribute
+ *
+ * There are a valid reasons why a webmaster may not want this WordPress plugin
+ *  adding the SRI tag. For example, they may have a different plugin that already
+ *  does that from a database. In such cases, the CrossOrigin attribute should
+ *  still be added, along with the fallback HTML snippet. This function does that.
+ *  See the PHPdoc header for pipjqui_add_sri_attributes().
+ *
+ * @param string The original version of the script tag.
+ * @param string The handle that was used to register the script associated with
+ *               the tag in the first parameter.
+ * @param string The contents of the `src` attribute in the tag from the first
+ *               parameter.
+ *
+ * @return string
+ */
+function pipjqui_add_crossorigin_attribute( string $tag, string $handle, string $source ): string
+{
+  if ( $handle === 'jquery-ui-core' ) {
+    $html = pipjq_fallback_for_cdn_failure();
+    return '<script src="' . $source . '" crossorigin="anonymous"></script>' . PHP_EOL . $html;
+  }
+  return $tag;
+}
 
 function pipjq_update_wpcore_jqueryui()
 {
+  $sri     = pipjqui_get_option_as_boolean( 'pipjqui_sri' );
+  $cdnhost = pipjqui_get_cdnhost();
+  
+  $srcuri = pipjqui_script_src( $cdnhost );
+  
 
   $core = array(         'jquery-ui-widget',
                          'jquery-ui-position');
@@ -326,6 +445,14 @@ function pipjq_update_wpcore_jqueryui()
   foreach( $alias_components as $component ) {
     wp_register_script( $component, false, array( 'jquery-ui-core' ), null );
   }
+  if ( $srcuri->cdn ) {
+    if ( $sri ) {
+      add_filter( 'script_loader_tag', 'pipjqui_add_sri_attributes', 10, 3 );
+    } else {
+      add_filter( 'script_loader_tag', 'pipjqui_add_crossorigin_attribute', 10, 3 );
+    }
+  }
+}
   
 /**
  * Load default jQuery UI theme
