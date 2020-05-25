ab191ec8f972329f4163608cb591131636968a99
pipfrosch <pipfroschpress@gmail.com>
2020-05-25 08:51:45 -0700

    typo (doh!)

--- pipfrosch-jquery-ui.php
+++ pipfrosch-jquery-ui.php
@@ -48,7 +48,7 @@ if ( is_admin() ) {
   $demo = true;
   if ( $demo ) {
     wp_register_style( 'pipjqui-democss', PIPJQUI_PLUGIN_WEBPATH . 'demo/pipjqui-demo.css', array(), '1' );
-    wp_register_script( 'pipjqui-demojs', PIPJQUI_PLUGIN_WEBPATH . 'demo/pipjqui-demo.css', array('jquery-ui-core'), '1', true );
+    wp_register_script( 'pipjqui-demojs', PIPJQUI_PLUGIN_WEBPATH . 'demo/pipjqui-demo.js', array('jquery-ui-core'), '1', true );
     add_action( 'wp_enqueue_scripts', 'pipjqui_shortcode_style');
     add_action( 'init', 'pipjqui_register_shortcodes');
   }
