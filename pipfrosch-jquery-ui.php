3ae5472b9b4f4bc4189df40d60b1732727c064fc
pipfrosch <pipfroschpress@gmail.com>
2020-05-26 08:25:55 -0700

    temporarily intentionally break

--- pipfrosch-jquery-ui.php
+++ pipfrosch-jquery-ui.php
@@ -47,7 +47,7 @@ if ( is_admin() ) {
   //  as a dependency for a script, I have to test. Should be able to.
   // If I can then all I have to do is specify jquery-ui-theme-active as a
   //  dependency of jquery-ui-core and I do not need pipjqui_load_default_theme()
-  add_action( 'wp_enqueue_scripts', 'pipjqui_load_default_theme', PHP_INT_MAX );
+//  add_action( 'wp_enqueue_scripts', 'pipjqui_load_default_theme', PHP_INT_MAX );
   // for the demo page - change below to false once I have menu working
   $demo = pipjqui_get_option_as_boolean( 'pipjqui_demo', false );
   if ( $demo ) {
