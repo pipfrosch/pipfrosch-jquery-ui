cc39827f22e505cc99e0b845c4ef68b7d1bd4f3e
pipfrosch <pipfroschpress@gmail.com>
2020-05-27 08:40:08 -0700

    All CDNs have minified, do not need SRI for non.

--- themes/mksri.sh
+++ themes/mksri.sh
@@ -5,10 +5,7 @@ for theme in base black-tie blitzer cupertino dark-hive dot-luv eggplant excite-
   pwd
   cd ${theme}
   pwd
-  echo "jquery-ui.css:     sha256-`shasum -b -a 256 "jquery-ui.css"     |awk '{ print $1 }' |xxd -r -p |base64`" >  sri.sha256.txt
-  echo "jquery-ui.min.css: sha256-`shasum -b -a 256 "jquery-ui.min.css" |awk '{ print $1 }' |xxd -r -p |base64`" >> sri.sha256.txt
-
-  echo "jquery-ui.css:     sha384-`shasum -b -a 384 "jquery-ui.css"     |awk '{ print $1 }' |xxd -r -p |base64`" >  sri.sha384.txt
-  echo "jquery-ui.min.css: sha384-`shasum -b -a 384 "jquery-ui.min.css" |awk '{ print $1 }' |xxd -r -p |base64`" >> sri.sha384.txt
+  echo "jquery-ui.min.css: sha256-`shasum -b -a 256 "jquery-ui.min.css" |awk '{ print $1 }' |xxd -r -p |base64`"  > sri.txt
+  echo "jquery-ui.min.css: sha384-`shasum -b -a 384 "jquery-ui.min.css" |awk '{ print $1 }' |xxd -r -p |base64`" >> sri.txt
   cd ..
 done
